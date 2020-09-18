4be9b9ad1456cd3982a24df1d2ca784345efe601
Nicholas Gibson <nckgibson4@gmail.com>
2020-09-18 10:38:27 -0700

    Updates

---
+++ precache-manifest.01244063b37500e4757556e3f2c7ddd6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b90027307007ed80ea134008d294dbc",
+    "url": "/hacker-news-clone/index.html"
+  },
+  {
+    "revision": "0a95d1ff14f1c18ffcf8",
+    "url": "/hacker-news-clone/static/js/2.01a2b292.chunk.js"
+  },
+  {
+    "revision": "863004d46cd7375f6c043c934b2ed4e3",
+    "url": "/hacker-news-clone/static/js/2.01a2b292.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c3532a3c9f479d13b0e",
+    "url": "/hacker-news-clone/static/js/main.5423e220.chunk.js"
+  },
+  {
+    "revision": "c9a3baf2b0bc3fd6620c",
+    "url": "/hacker-news-clone/static/js/runtime-main.033094b6.js"
+  }
+]);
